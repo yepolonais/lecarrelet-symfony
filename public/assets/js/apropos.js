b5645f6c5583d6a3518f2207f128f78e538b5c1b
Timothée <timothedarde33@gmail.com>
2020-04-02 10:51:25 +0200

    import version 90% inté timothée

--- public/assets/js/apropos.js
+++ public/assets/js/apropos.js
@@ -1,5 +1,40 @@
 $( function() {
 
+// affichage des blocs de formulaire
+  $("#btn-mail").click(function(){
+    $(".boutons").hide();
+    $(".retour").css("opacity","1");
+    $(".contact").css("position","relative");
+    $(".contact").animate(
+      {right:0},400);
+  });
+  $("#btn-tel").click(function(){
+    $(".boutons").hide();
+    $(".retour").css("opacity","1");
+    $(".rappel").css("position","relative");
+    $(".rappel").animate(
+      {right:0},400);
+  });
+  $(".retour").click(function(){
+    $(".boutons").show();
+    $(".retour").css("opacity","0");
+    if($(".rappel").css("right") == "0px")
+    {
+
+      $(".rappel").css("position","absolute");
+      $(".rappel").animate(
+        {right:"120%"},0);
+    }
+    else{}
+    if($(".contact").css("right") == "0px")
+    {
+      $(".contact").css("position","absolute");
+      $(".contact").animate(
+        {right:"120%"},0);
+    }
+    else{}
+  });
+
    /* Check width on page load*/
    $(document).ready(function(){
        if(window.innerWidth > 767){
