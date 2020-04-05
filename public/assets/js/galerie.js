a40386fb5ea5b05682ba4145cc6410395ed1f00b
yepolonais <58600334+yepolonais@users.noreply.github.com>
2020-04-06 01:24:07 +0200

    update galery

---
+++ public/assets/js/galerie.js
@@ -0,0 +1,7 @@
+let flecheGauche = document.getElementById('fleche-gauche');
+let flecheDroite = document.getElementById('fleche-droite');
+let photoCentre = document.getElementById('photo-centre');
+
+flecheGauche.addEventListener("click", photoPrecedente());
+flecheDroite.addEventListener("click", photoSuivante());
+photoCentre.addEventListener("click", photoSuivante());
