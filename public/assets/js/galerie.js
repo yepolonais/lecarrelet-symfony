7c4e1e2e1fdd34e2dbedd2694f224f345d52ce22
yepolonais <58600334+yepolonais@users.noreply.github.com>
2020-04-17 18:12:59 +0200

    update slider

--- public/assets/js/galerie.js
+++ public/assets/js/galerie.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel();
+    // $('.owl-carousel').owlCarousel();
+    var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    nav:true
+});
+$('.fleche-droite').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.fleche-gauche').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
 });
 
-let flecheGauche = document.getElementById('fleche-gauche');
-let flecheDroite = document.getElementById('fleche-droite');
-let photoCentre = document.getElementById('photo-centre');
 
-flecheGauche.addEventListener("click", photoPrecedente());
-flecheDroite.addEventListener("click", photoSuivante());
-photoCentre.addEventListener("click", photoSuivante());
+
+
+
+
+
+
+
+
+
+
+// let flecheGauche = document.getElementById('fleche-gauche');
+// let flecheDroite = document.getElementById('fleche-droite');
+// let photoCentre = document.getElementById('photo-centre');
+//
+// flecheGauche.addEventListener("click", photoPrecedente());
+// flecheDroite.addEventListener("click", photoSuivante());
+// photoCentre.addEventListener("click", photoSuivante());
