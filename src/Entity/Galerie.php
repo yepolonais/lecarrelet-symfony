d75c284df13c57d1c66e826e22974914578f180e
yepolonais <58600334+yepolonais@users.noreply.github.com>
2020-04-17 16:26:00 +0200

    rebase master

--- src/Entity/Galerie.php
+++ src/Entity/Galerie.php
@@ -24,21 +24,12 @@ class Galerie
     /**
      * @ORM\Column(type="datetime")
      */
-<<<<<<< HEAD
     private $datepublication;
 
     /**
      * @ORM\Column(type="text")
      */
     private $urlphoto;
-=======
-    private $date_publication;
-
-    /**
-     * @ORM\Column(type="string", length=1000)
-     */
-    private $url_photo;
->>>>>>> bdd
 
     public function getId(): ?int
     {
@@ -59,42 +50,24 @@ class Galerie
 
     public function getDatePublication(): ?\DateTimeInterface
     {
-<<<<<<< HEAD
         return $this->datepublication;
     }
 
     public function setDatePublication(\DateTimeInterface $datepublication): self
     {
         $this->datepublication = $datepublication;
-=======
-        return $this->date_publication;
-    }
-
-    public function setDatePublication(\DateTimeInterface $date_publication): self
-    {
-        $this->date_publication = $date_publication;
->>>>>>> bdd
 
         return $this;
     }
 
     public function getUrlPhoto(): ?string
     {
-<<<<<<< HEAD
         return $this->urlphoto;
     }
 
     public function setUrlPhoto(string $urlphoto): self
     {
         $this->urlphoto = $urlphoto;
-=======
-        return $this->url_photo;
-    }
-
-    public function setUrlPhoto(string $url_photo): self
-    {
-        $this->url_photo = $url_photo;
->>>>>>> bdd
 
         return $this;
     }
