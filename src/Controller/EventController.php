d3156aacce560dd37047a92218f73a35ffcd5e70
unknown <nicolaslepetit@hotmail.fr>
2020-04-18 22:03:22 +0200

    update

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,18 +1,21 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Evenement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EvenementRepository;
 
 class EventController extends AbstractController
 {
     /**
      * @Route("/event", name="event")
      */
-    public function index()
+    public function index(EvenementRepository $response)
     {
+        $evenements = $response->findAll();
         return $this->render('event/event.html.twig', [
+          'evenements' => $evenements,
             'controller_name' => 'EventController',
         ]);
     }
