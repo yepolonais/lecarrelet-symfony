79e915fc29420713c81006f00b02049c9d995a01
Timothée <timothedarde33@gmail.com>
2020-04-20 14:28:32 +0200

    tri des event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -12,15 +12,14 @@ class EventController extends AbstractController
     /**
      * @Route("/event", name="event")
      */
+
     public function index(EvenementRepository $response)
     {
-        $evenements = $response->findAll();
-
-        return $this->render('event/event.html.twig', [
+        $evenements = $response->findBy(array(), array('debut' => 'ASC'));
+            return $this->render('event/event.html.twig', [
             'evenements' => $evenements,
             'controller_name' => 'EventController',
         ]);
     }
-
 
 }
