d904a9f81e6f2e86eb8b6f17dd0887a65195592f
unknown <nicolaslepetit@hotmail.fr>
2020-04-21 22:27:15 +0200

    recuperation des soirees dans le form

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Evenement;
+use App\Repository\EvenementRepository;
 
 class FormController extends AbstractController
 {
     /**
      * @Route("/form", name="form")
      */
-    public function index()
+    public function index(EvenementRepository $response)
     {
+      $evenements = $response->findAll();
         return $this->render('form/form.html.twig', [
+          'evenements' => $evenements,
             'controller_name' => 'FormController',
         ]);
     }
