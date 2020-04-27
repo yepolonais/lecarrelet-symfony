f976682593f66b9d0a75e92f96b89514faea2393
yepolonais <58600334+yepolonais@users.noreply.github.com>
2020-04-27 17:12:49 +0200

    photo galerie update

--- src/Entity/Galerie.php
+++ src/Entity/Galerie.php
@@ -19,62 +19,11 @@ class Galerie
      * @ORM\Column(type="integer")
      */
     private $id;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
     public $titre;
-
-// upload photo -------------------------------------------------------//
-
-/**
-    * @ORM\Column(type="string", length=255)
-    * @var string
-    */
-   private $image;
-
-   /**
-    * @Vich\UploadableField(mapping="images_galerie", fileNameProperty="image")
-    * @var File
-    */
-   private $imageFile;
-
-   /**
-    * @ORM\Column(type="datetime")
-    * @var \DateTime
-    */
-   private $updatedAt;
-
-   // ...
-
-   public function setImageFile(File $image = null)
-   {
-       $this->imageFile = $image;
-
-       // VERY IMPORTANT:
-       // It is required that at least one field changes if you are using Doctrine,
-       // otherwise the event listeners won't be called and the file is lost
-       if ($image) {
-           // if 'updatedAt' is not defined in your entity, use another property
-           $this->updatedAt = new \DateTime('now');
-       }
-   }
-
-   public function getImageFile()
-   {
-       return $this->imageFile;
-   }
-
-   public function setImage($image)
-   {
-       $this->image = $image;
-   }
-
-   public function getImage()
-   {
-       return $this->image;
-   }
-
-// upload photo -------------------------------------------------------fin//
 
     public function getId(): ?int
     {
@@ -92,4 +41,59 @@ class Galerie
 
         return $this;
     }
+
+// upload photo -------------------------------------------------------//
+
+/**
+    * @ORM\Column(type="string", length=255)
+    * @var string
+    */
+   private $image;
+
+   /**
+    * @Vich\UploadableField(mapping="images_galerie", fileNameProperty="image")
+    * @var File
+    */
+   private $imageFile;
+
+   // /**
+   //  * @ORM\Column(type="datetime")
+   //  * @var \DateTime
+   //  */
+   // private $updatedAt;
+
+   // ...
+
+   public function setImageFile(File $image = null)
+   {
+       $this->imageFile = $image;
+
+       // VERY IMPORTANT:
+       // It is required that at least one field changes if you are using Doctrine,
+       // otherwise the event listeners won't be called and the file is lost
+       // if ($image) {
+       //     // if 'updatedAt' is not defined in your entity, use another property
+       //     $this->updatedAt = new \DateTime('now');
+       // }
+   }
+
+   public function getImageFile()
+   {
+       return $this->imageFile;
+   }
+
+   public function setImage($image)
+   {
+       $this->image = $image;
+   }
+
+   public function getImage()
+   {
+       // return 'assets/img/Galerie/' .  $this->image;
+       return $this->image;
+   }
+
+// upload photo -------------------------------------------------------fin//
+
+
 }
